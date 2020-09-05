5ef508d0aaae78c5d931a7d2c7784bb4df33add4
amSahar <saharmohad96@gmail.com>
2020-09-06 02:09:43 +0300

    Completed module

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = new express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.listen (3000);
-// console.log ("PS Project Running on port 3000!");
+app.listen (3000);
+console.log ("PS Project Running on port 3000!");
